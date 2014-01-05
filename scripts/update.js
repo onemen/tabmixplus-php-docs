9ef6aa43e3797a8b85a1b19c9cbbcd598e95aefd
onemen <tabmix.onemen@gmail.com>
2014-01-05 13:48:49 +0200

    Remove debug leftover

--- scripts/update.js
+++ scripts/update.js
@@ -7,18 +7,6 @@
       window.document.body.classList.remove("innerFrame")
     else
       window.parent.document.title = window.document.title;
-
-return;
-    var title = "";
-    if (href.indexOf("changelog") > -1)
-      title = "Change Log";
-    else if (href.indexOf("troubleshooting") > -1)
-      title = "Troubleshooting";
-
-    var topFrame = window.parent.document.getElementById("top-frame");
-console.info(topFrame && topFrame.src);
-    if (topFrame && topFrame.src.indexOf("?title") == -1 && title)
-      window.parent.document.getElementById("top-frame").src += "&title=" + title;
   }
 
   window.addEventListener("DOMContentLoaded", contentLoaded, false);
