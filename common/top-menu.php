96ce6fee93f48fda3ada34dd71af99a2a1bdfec9
onemen <tabmix.onemen@gmail.com>
2014-01-06 20:55:15 +0200

    Fix path in top-menu.php
    --HG--
    branch : default-php

--- common/top-menu.php
+++ common/top-menu.php
@@ -1,5 +1,9 @@
+<?php
+echo <<<html
 <a href="http://tmp.garyr.net/forum/index.php">Forum</a>
         <a href="http://tmp.garyr.net/help/index.html">Help</a>
-        <a href="http://tmp.garyr.net/support/troubleshooting/index.html">Troubleshooting</a>
-        <a href="http://tmp.garyr.net/support/changelog/index.php">Change Log</a>
+        <a href="$PATH/troubleshooting/index.php">Troubleshooting</a>
+        <a href="$PATH/changelog/index.php">Change Log</a>
         <a href="https://addons.mozilla.org/en-US/firefox/addon/tab-mix-plus/">Download</a>
+html;
+?>
