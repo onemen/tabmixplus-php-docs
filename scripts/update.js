5a7e0d0405df43ed67db4f823e0de916220fc90c
onemen <tabmix.onemen@gmail.com>
2014-01-17 10:53:00 +0200

    Missing semicolon

--- scripts/update.js
+++ scripts/update.js
@@ -4,7 +4,7 @@
     var href = window.parent.location.href;
     // frame opened in new tab
     if (href.indexOf("viewpage.php") > -1)
-      window.document.body.classList.remove("innerFrame")
+      window.document.body.classList.remove("innerFrame");
 
     window.parent.document.title = document.getElementById("title").textContent;
   }
