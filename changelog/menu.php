6a945a3092c1aa2858f79ed6a1fec40b08283063
onemen <tabmix.onemen@gmail.com>
2014-02-11 10:02:36 +0200

    Add Current version label to 0.4.1.3

--- changelog/menu.php
+++ changelog/menu.php
@@ -1,12 +1,13 @@
     <div class="th">Versions List</div>
     <div class="menu-body">
       <div class="topic">
-        <h4>Versions 0.4.0.0+</h4>
+        <h4><u>Versions 0.4.0.0+</u></h4>
 <?php
   $href = "viewpage.php?t=1&p";
   $target = 'target="tabmix-changelog-content"';
+  // <p><a href="$href=0.4.1.4" $target>0.4.1.4 - <span style="font-size: 0.9em;">Development version</span></a></p>
 echo <<<html
-        <p><a href="$href=0.4.1.3" $target>0.4.1.3</a></p>
+        <p><a href="$href=0.4.1.3" $target>0.4.1.3 - <span style="font-size: 0.9em;">Current version</span></a></p>
         <p><a href="$href=0.4.1.2" $target>0.4.1.2</a></p>
         <p><a href="$href=0.4.1.1" $target>0.4.1.1</a></p>
         <p><a href="$href=0.4.1.0" $target>0.4.1.0</a></p>
@@ -20,7 +21,7 @@ echo <<<html
 html;
 ?>
 
-        <h4>Old versions changes</h4>
+        <h4><u>Old versions changes</u></h4>
         <p>versions <a href="http://tmp.garyr.net/forum/viewtopic.php?t=10888">0.3.8.2 - 0.3.8.7</a></p>
         <p>version   <a href="http://tmp.garyr.net/forum/viewtopic.php?f=3&t=9864">0.3.7.4</a></p>
         <p>versions <a href="http://tmp.garyr.net/forum/viewtopic.php?f=3&t=9178">0.3.6.1 - 0.3.7</a></p>
