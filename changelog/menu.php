2c3517b1c028ef2bed2529459a40353cb1194758
onemen <tabmix.onemen@gmail.com>
2016-03-28 18:09:23 +0300

    Changelog for 0.4.2.3pre.1600319a1

--- changelog/menu.php
+++ changelog/menu.php
@@ -3,13 +3,11 @@
       <div class="topic changelog">
         <h4><u>Versions 0.4.1.1+</u></h4>
 
-<!--
-<p><a href="$href=0.4.2.1" $target>0.4.2.1 - <span style="font-size: 0.9em;">Development version</span></a></p>
--->
 <?php
   $href = "viewpage.php?t=1&p";
   $target = 'target="tabmix-changelog-content"';
 echo <<<html
+        <p><a href="$href=0.4.2.3" $target>0.4.2.3 - <span style="font-size: 0.9em;">Development version</span></a></p>
         <p><a href="$href=0.4.2.2" $target>0.4.2.2 - <span style="font-size: 0.9em;">Current version</span></a></p>
         <p><a href="$href=0.4.2.1" $target>0.4.2.1</a></p>
         <p><a href="$href=0.4.2.0" $target>0.4.2.0</a></p>
--- common/version_data.php
+++ common/version_data.php
@@ -1,6 +1,6 @@
 <?php
   $VERSION = "0.4.2.2";
-  $DEVBUILD = "0.4.2.1pre.160228a2";
-  $DEVBUILDPATH = "https://addons.mozilla.org/firefox/downloads/file/406033/tab_mix_plus-0.4.2.1pre.160228a2-fx.xpi";
-  $LATEST = "0.4.2.2";
+  $DEVBUILD = "0.4.2.3pre.160319a1";
+  $DEVBUILDPATH = "https://addons.mozilla.org/firefox/downloads/file/413882/tab_mix_plus-0.4.2.3pre.160319a1-fx.xpi";
+  $LATEST = "0.4.2.3";
 ?>
