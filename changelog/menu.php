ef8657994d063bb5fcad31b87afbcdcd2f4bd96e
onemen <tabmix.onemen@gmail.com>
2016-06-12 12:47:28 +0300

    Changelog for 0.5.0.0pre.160522a1, update official version to 0.5.0.0

--- changelog/menu.php
+++ changelog/menu.php
@@ -1,29 +1,30 @@
     <div class="th">Versions List</div>
     <div class="menu-body">
       <div class="topic changelog">
-        <h4><u>Versions 0.4.1.1+</u></h4>
+        <h4><u>Versions 0.4.2.0+</u></h4>
 
 <?php
   $href = "viewpage.php?t=1&p";
   $target = 'target="tabmix-changelog-content"';
 echo <<<html
-        <p><a href="$href=0.5.0.0" $target>0.5.0.0 - <span style="font-size: 0.9em;">Development version</span></a></p>
-        <p><a href="$href=0.4.2.2" $target>0.4.2.2 - <span style="font-size: 0.9em;">Current version</span></a></p>
+        <p><a href="$href=0.5.0.1" $target>0.5.0.1 - <span style="font-size: 0.9em;">Development version</span></a></p>
+        <p><a href="$href=0.5.0.0" $target>0.5.0.0 - <span style="font-size: 0.9em;">Current version</span></a></p>
+        <p><a href="$href=0.4.2.2" $target>0.4.2.2</a></p>
         <p><a href="$href=0.4.2.1" $target>0.4.2.1</a></p>
         <p><a href="$href=0.4.2.0" $target>0.4.2.0</a></p>
-        <p><a href="$href=0.4.1.9" $target>0.4.1.9</a></p>
-        <p><a href="$href=0.4.1.8" $target>0.4.1.8</a></p>
-        <p><a href="$href=0.4.1.7" $target>0.4.1.7</a></p>
-        <p><a href="$href=0.4.1.6" $target>0.4.1.6</a></p>
-        <p><a href="$href=0.4.1.5" $target>0.4.1.5</a></p>
-        <p><a href="$href=0.4.1.4" $target>0.4.1.4</a></p>
-        <p><a href="$href=0.4.1.3" $target>0.4.1.3</a></p>
-        <p><a href="$href=0.4.1.2" $target>0.4.1.2</a></p>
-        <p><a href="$href=0.4.1.1" $target>0.4.1.1</a></p>
         <h4 class="text-link" onclick="toggleMenu('version0.4.1');" style="margin-top:5px;">
-          <u>Versions 0.4.0.0 - 0.4.1.0</u>
+          <u>Versions 0.4.0.0 - 0.4.1.9</u>
         </h4>
         <div id="version0.4.1" collapsed="true">
+          <p><a href="$href=0.4.1.9" $target>0.4.1.9</a></p>
+          <p><a href="$href=0.4.1.8" $target>0.4.1.8</a></p>
+          <p><a href="$href=0.4.1.7" $target>0.4.1.7</a></p>
+          <p><a href="$href=0.4.1.6" $target>0.4.1.6</a></p>
+          <p><a href="$href=0.4.1.5" $target>0.4.1.5</a></p>
+          <p><a href="$href=0.4.1.4" $target>0.4.1.4</a></p>
+          <p><a href="$href=0.4.1.3" $target>0.4.1.3</a></p>
+          <p><a href="$href=0.4.1.2" $target>0.4.1.2</a></p>
+          <p><a href="$href=0.4.1.1" $target>0.4.1.1</a></p>
           <p><a href="$href=0.4.1.0" $target>0.4.1.0</a></p>
           <p><a href="$href=0.4.0.5" $target>0.4.0.5</a></p>
           <p><a href="$href=0.4.0.4" $target>0.4.0.4</a></p>
--- common/version_data.php
+++ common/version_data.php
@@ -1,7 +1,7 @@
 <?php
-  $VERSION = "0.4.2.2";
-  $DEVBUILD = "0.5.0.0pre.160515a1";
-  $DEVBUILDPATH = "https://addons.mozilla.org/firefox/downloads/file/438533/tab_mix_plus-0.5.0.0pre.160515a1-fx.xpi";
+  $VERSION = "0.5.0.0";
+  $DEVBUILD = "0.5.0.0pre.160522a1";
+  $DEVBUILDPATH = "https://addons.mozilla.org/firefox/downloads/file/441525/tab_mix_plus-0.5.0.0pre.160522a1-fx.xpi";
   $LATEST = "0.5.0.0";
   $MINFRIEFOXVERSION = "31.0";
   $MAXFRIEFOXVERSION = "Nightly";
