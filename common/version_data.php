9a624efcd471771a5d1cfc5c034416f257031685
onemen <tabmix.onemen@gmail.com>
2017-08-29 13:24:35 +0300

    Update official version to 0.5.0.4

--- common/version_data.php
+++ common/version_data.php
@@ -1,8 +1,8 @@
 <?php
-  $VERSION = "0.5.0.3";
+  $VERSION = "0.5.0.4";
   $DEVBUILD = "0.5.0.5pre.170827a1";
   $DEVBUILDPATH = "https://addons.mozilla.org/firefox/downloads/file/710569/tab_mix_plus-0.5.0.5pre.170827a1-fx.xpi";
-  $LATEST = "0.5.0.4";
+  $LATEST = "0.5.0.5";
   $MINFRIEFOXVERSION = "38.0";
   $MAXFRIEFOXVERSION = "Nightly";
 ?>
