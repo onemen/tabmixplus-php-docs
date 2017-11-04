067b7f9414531b473cca26977bbd6a4285eab83f
onemen <tabmix.onemen@gmail.com>
2017-11-04 20:30:44 +0200

    Changelog for 0.5.0.5pre.171027a1

--- common/version_data.php
+++ common/version_data.php
@@ -1,8 +1,8 @@
 <?php
   $VERSION = "0.5.0.4";
-  $DEVBUILD = "tab_mix_plus-0.5.0.5pre.170827a2";
-  $DEVBUILDPATH = "https://addons.mozilla.org/firefox/downloads/file/718700/tab_mix_plus-0.5.0.5pre.170827a2-fx.xpi";
+  $DEVBUILD = "tab_mix_plus-0.5.0.5pre.171027a1";
+  $DEVBUILDPATH = "https://addons.mozilla.org/firefox/downloads/file/753107/tab_mix_plus-0.5.0.5pre.171027a1-fx.xpi";
   $LATEST = "0.5.0.5";
-  $MINFRIEFOXVERSION = "38.0";
+  $MINFRIEFOXVERSION = "45.0";
   $MAXFRIEFOXVERSION = "Nightly";
 ?>
