c6e1866adc69bb17df8dff1ff6e1f70231726269
onemen <tabmix.onemen@gmail.com>
2018-07-13 07:34:35 +0300

    Changelog for 0.5.0.5pre.171209a1

--- common/version_data.php
+++ common/version_data.php
@@ -1,7 +1,7 @@
 <?php
   $VERSION = "0.5.0.4";
-  $DEVBUILD = "tab_mix_plus-0.5.0.5pre.171209a1";
-  $DEVBUILDPATH = "https://bitbucket.org/onemen/tabmixplus/downloads/tab_mix_plus-0.5.0.5pre.171209a1.xpi";
+  $DEVBUILD = "tab_mix_plus-0.5.0.5pre.180526a1.xpi";
+  $DEVBUILDPATH = "https://bitbucket.org/onemen/tabmixplus/downloads/tab_mix_plus-0.5.0.5pre.180526a1.xpi";
   $LATEST = "0.5.0.5";
   $MINFRIEFOXVERSION = "45.0";
   $MAXFRIEFOXVERSION = "Nightly";
