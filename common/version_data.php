5aca5cf94013fcb937064fce93b653bb6e7f2297
onemen <tabmix.onemen@gmail.com>
2019-02-07 11:22:50 +0200

    Update for version 0.5.6.0

--- common/version_data.php
+++ common/version_data.php
@@ -1,8 +1,8 @@
 <?php
-  $VERSION = "0.5.5.0";
-  $DEVBUILD = "tab_mix_plus-0.5.5.0.xpi";
-  $DEVBUILDPATH = "https://bitbucket.org/onemen/tabmixplus/downloads/tab_mix_plus-0.5.5.0.xpi";
-  $LATEST = "0.6.0.0";
+  $VERSION = "0.5.6.0";
+  $DEVBUILD = "tab_mix_plus-0.5.6.0.xpi";
+  $DEVBUILDPATH = "https://bitbucket.org/onemen/tabmixplus/downloads/tab_mix_plus-0.5.6.0-fx.xpi";
+  $LATEST = "0.5.6.0";
   $MINFRIEFOXVERSION = "45.0";
   $MAXFRIEFOXVERSION = "56.*";
 ?>
