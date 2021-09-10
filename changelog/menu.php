3634da94b389338d6304184ab387a69fcfab4980
onemen <tabmix.onemen@gmail.com>
2021-09-10 08:38:02 +0300

    Update for version 1.0.0

--- changelog/menu.php
+++ changelog/menu.php
@@ -1,21 +1,27 @@
     <div class="th">Versions List</div>
     <div class="menu-body">
       <div class="topic changelog">
-        <h4><u>Versions 0.5.0.0+</u></h4>
+        <h4><u>Versions 1.0.0+</u></h4>
 
 <?php
   $href = "viewpage.php?t=1&p";
   $target = 'target="tabmix-changelog-content"';
 echo <<<html
-        <p><a href="$href=0.5.8.1" $target>0.5.8.1 - <span style="font-size: 0.9em;">Current version</span></a></p>
-        <p><a href="$href=0.5.7.0" $target>0.5.7.0</a></p>
-        <p><a href="$href=0.5.6.0" $target>0.5.6.0</a></p>
-        <p><a href="$href=0.5.5.0" $target>0.5.5.0</a></p>
-        <p><a href="$href=0.5.0.4" $target>0.5.0.4</a></p>
-        <p><a href="$href=0.5.0.3" $target>0.5.0.3</a></p>
-        <p><a href="$href=0.5.0.2" $target>0.5.0.2</a></p>
-        <p><a href="$href=0.5.0.1" $target>0.5.0.1</a></p>
-        <p><a href="$href=0.5.0.0" $target>0.5.0.0</a></p>
+        <p><a href="$href=1.0.0" $target>1.0.0 - <span style="font-size: 0.9em;">Development version</span></a></p>
+        <h4 class="has-submenu text-link" style="margin-top:5px;">
+          <u>Versions 0.5.0.0 - 0.5.8.1</u>
+        </h4>
+        <div class="submenu">
+          <p><a href="$href=0.5.8.1" $target>0.5.8.1 - <span style="font-size: 0.9em;">Current version</span></a></p>
+          <p><a href="$href=0.5.7.0" $target>0.5.7.0</a></p>
+          <p><a href="$href=0.5.6.0" $target>0.5.6.0</a></p>
+          <p><a href="$href=0.5.5.0" $target>0.5.5.0</a></p>
+          <p><a href="$href=0.5.0.4" $target>0.5.0.4</a></p>
+          <p><a href="$href=0.5.0.3" $target>0.5.0.3</a></p>
+          <p><a href="$href=0.5.0.2" $target>0.5.0.2</a></p>
+          <p><a href="$href=0.5.0.1" $target>0.5.0.1</a></p>
+          <p><a href="$href=0.5.0.0" $target>0.5.0.0</a></p>
+        </div>
         <h4 class="has-submenu text-link" style="margin-top:5px;">
           <u>Versions 0.4.0.0 - 0.4.2.2</u>
         </h4>
