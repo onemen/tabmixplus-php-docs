51f50acf18c32c46c7a41f3d3a42f71e7d880f33
onemen <tabmix.onemen@gmail.com>
2022-03-17 13:41:36 +0200

    chore: update firefox max version

--- common/version_data.php
+++ common/version_data.php
@@ -4,5 +4,5 @@
   $DEVBUILDPATH = "https://github.com/onemen/TabMixPlus/releases/download/v1.0.0-pre.6/tab_mix_plus-dev-build.xpi";
   $LATEST = "1.0.0-pre";
   $MINFRIEFOXVERSION = "78.0";
-  $MAXFRIEFOXVERSION = "94.*";
+  $MAXFRIEFOXVERSION = "100.*";
 ?>
