174cf5c8c1f06c3482f3bc61c4ff975888460557
onemen <tabmix.onemen@gmail.com>
2022-11-18 08:14:53 +0200

    chore: Changelog v1.0.0-pre.11

--- common/version_data.php
+++ common/version_data.php
@@ -1,7 +1,7 @@
 <?php
   $VERSION = "1.0.0-pre";
   $DEVBUILD = "tab_mix_plus-dev-build.xpi";
-  $DEVBUILDPATH = "https://github.com/onemen/TabMixPlus/releases/download/v1.0.0-pre.9/tab_mix_plus-dev-build.xpi";
+  $DEVBUILDPATH = "https://github.com/onemen/TabMixPlus/releases/download/v1.0.0-pre.11/tab_mix_plus-dev-build.xpi";
   $LATEST = "1.0.0-pre";
   $MINFRIEFOXVERSION = "78.0";
   $MAXFRIEFOXVERSION = "100.*";
